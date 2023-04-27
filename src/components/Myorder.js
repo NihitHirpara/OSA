87a70d8968a11acc27f8ca05fe99b75a68ec02d7
NIHIT <nihitpatel88@gmail.com>
2023-04-27 13:06:01 +0530

    my order responsive

--- src/components/Myorder.js
+++ src/components/Myorder.js
@@ -19,23 +19,31 @@ const Wrapper = styled.div`
   }
   ul {
     display: flex;
-    justify-content: space-around;
     margin-bottom: 50px;
+    justify-content: center;
     margin-top: 50px;
   }
   .img-section {
     display: flex;
     flex-direction: column;
   }
+  .product ul li {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 5px 0;
+    border: 1px solid gray;
+    width: 120px;
+  }
 
-  @media (max-width: 767px) {
-    ul {
-      flex-direction: column;
-      align-items: center;
-      justify-content: start;
-      gap: 15px;
-    }
-
+  .product ul {
+    margin: 0;
+  }
+  .product {
+    text-align: center;
+    margin: 30px 0;
+  }
+  @media (max-width: 768px) {
     .img-section {
       flex-direction: row;
     }
@@ -45,6 +53,25 @@ const Wrapper = styled.div`
 
     p {
       display: inline-flex;
+    }
+    .product ul li {
+      width: 100px;
+    }
+  }
+  @media (max-width: 520px) {
+    ul {
+      justify-content: space-around;
+    }
+    .product ul {
+      display: inline-flex;
+      flex-direction: column;
+    }
+    .product ul li {
+      width: 100%;
+      height: 50px;
+    }
+    .heading {
+      font-size: 4rem;
     }
   }
 `;
@@ -71,38 +98,30 @@ const Myorder = () => {
       <div>
         <h1 className="heading">Myorder</h1>
       </div>
-      <div>
-        {cart.map((item) => (
-          <ul>
+
+      <div className="product">
+        <ul>
+          <li>S. No.</li>
+          <li>item</li>
+          <li>image</li>
+          <li>order total</li>
+          <li>Quantity</li>
+        </ul>
+        {cart.map((item, index) => (
+          <ul key={item.id}>
+            <li>{index + 1}</li>
+            <li>{item.name}</li>
             <li>
-              <p>
-                item: <p>{item.name}</p>{" "}
-              </p>
-            </li>
-            <li className="img-data">
-              <p className="img-section">
-                image:
-                <img
-                  src={item.image}
-                  alt=""
-                  srcset=""
-                  className="img-section"
-                />
-              </p>
-            </li>
-            <li>
-              <p>
-                order total:
-                <p>
-                  <FormatPrice price={item.price * item.amount} />
-                </p>
-              </p>
+              <img
+                src={item.image}
+                alt="product_image"
+                className="img-section"
+              />
             </li>
             <li>
-              <p>
-                Quantity: <p>{item.amount}</p>{" "}
-              </p>
+              <FormatPrice price={item.price * item.amount} />
             </li>
+            <li>{item.amount}</li>
           </ul>
         ))}
       </div>
